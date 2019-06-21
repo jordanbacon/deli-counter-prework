9b8eeff465f945bced39c9766459d39392236cd4
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 01:20:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,1 @@
-# Write your code here.
+def line(katz_deli)  katz_deli = []  if katz_deli.length = 0    return "The line is currently empty."  else    return "The line is currently katz_deli."  endend
