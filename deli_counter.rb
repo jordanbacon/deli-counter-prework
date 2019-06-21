1cd0ad807552b7c0706803bb74bb3935b18ef75b
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 01:32:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   else
     current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |customer, index|
-      current_line << "#{index}. #{customer}"
+      current_line << " #{index}. #{customer}"
     end
     puts current_line
   end
