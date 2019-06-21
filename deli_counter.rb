82785a8980ce37a1364034520cf3a9199b9f9b0b
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 01:21:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,1 @@
-def line(katz_deli)  katz_deli = []  if katz_deli.length = 0    return "The line is currently empty."  else    return "The line is currently katz_deli."  endend
+katz_deli = []def line(x)  line_array = []  if x.length == 0    puts "The line is currently empty."  else    x.each.with_index(1) do |name, index|      line_array.push("#{index}. #{name}")    end    puts "The line is currently: #{line_array.join(" ")}"  endend
